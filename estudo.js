3ce0c9f851c1b5e0988fac6261222c2a6cb49877
lucas0520 <108934637+lucas0520@users.noreply.github.com>
2022-10-19 12:46:23 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -45,4 +45,18 @@ function total(){
   }
   
   document.write("resultado: " + r);
+
+      document.write("resultado:R$" + r);
+
+      function media(){
+        let n1 = document.getElementById("n1").value;
+        let n2 = document.getElementById("n2").value;
+        let n3 = document.getElementById("n3").value;
+        let n4 = document.getElementById("n4").value;
+
+        let r = (Number(n1) + Number (n2) + Number (n3) + Number (n4))/4;
+        document.getElementById("result").innerHTML = "media: " +r;
+      }
+
+
 }
