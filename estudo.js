076db117f2081d4000a51b33da133122463f0581
lucas0520 <108934637+lucas0520@users.noreply.github.com>
2022-10-21 09:07:52 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -33,10 +33,40 @@ function multiplica (){
       document.write("<br>")
   }
 }
+function moeda(atual){
+   return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+}
 function total(){
   let v = document.getElementById("valor").value;
   let j = document.getElementById("juros").value;
   let t = document.getElementById("meses").value;
+
+   if(!Number(v)){
+     alert("o valor deve ser numérico")
+     document.getElementById("valor").value = "";
+     document.getElementById("valor").focus();
+     return
+   
+  }
+
+  if(!Number(j)){
+    alert("o valor deve ser numérico")
+    document.getElementById("juros").value = "";
+    document.getElementById("juros").focus();
+    return
+  
+ }
+
+ if(!Number(t)){
+  alert("o valor deve ser numérico")
+  document.getElementById("meses").value = "";
+  document.getElementById("meses").focus();
+  return
+
+}
+
+ 
+
   let r = 0;
   for(let i=1; i <= t; i++){
      r = v * (1+(j/100));
@@ -44,9 +74,9 @@ function total(){
      v = r;
   }
   
-  document.write("resultado: " + r);
+  document.write("resultado: " + moeda(r));
 
-      document.write("resultado:R$" + r);
+      document.write("resultado:R$" + moeda(r));
 
       
 
@@ -60,5 +90,15 @@ function media(){
   let n4 = document.getElementById("n4").value;
 
   let r = (Number(n1)+Number (n2)+Number (n3)+Number (n4))/4;
-  document.getElementById("result").innerHTML = "media: " +r;
+  document.getElementById("result").innerHTML = "media: " +moeda(r);
+}
+function soma(){
+let n1 = document.getElementById("n1").value;
+let n2 = document.getElementById("n2").value;
+let n3 = document.getElementById("n3").value;
+let n4 = document.getElementById("n4").value;
+
+
+  let moeda = (Number(n1)+Number (n2)+Number (n3)+Number (n4));
+  document.getElementById("result").innerHTML = "soma: " +moeda(r);
 }
